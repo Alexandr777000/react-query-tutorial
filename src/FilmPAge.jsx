6d873b9484c310687ce6f0f659ef9a5415397f5b
Aleksandr_UB <bogdanov777000@mail.ru>
2023-04-25 19:18:30 +0300

    инишлстейт initialData

--- src/FilmPAge.jsx
+++ src/FilmPAge.jsx
@@ -1,27 +1,43 @@
 import React from "react";
-import {useQuery} from "react-query";
+import {useQuery, useQueryClient} from "react-query";
 
 const useGetFilm = (url) => {
+    const queryClient = useQueryClient();
+
     const {data} = useQuery(["film", url], () =>
-        fetch(url).then(res => res.json())
-    );
+        new Promise((resolve) => setTimeout(resolve, 2000))
+            .then(() =>
+                fetch(url)
+                    .then((res) => res.json())
+            ), {
+        // getQueryData -- на вход принимает ключ который хотим вытащить
+        // увидим наши фильмы быстрее чем подгрузится сама страница начальное значение
+        // пользователь сразу данные увидит!
+        // инишлДата не является читстым кодом - каждый раз итерауии делать придется..проходить по всему кешу искать фильм и отрисовывать его
+        initialData: queryClient.getQueryData("films")?.results?.find(film => film.url === url)
+    });
+
     return {data};
 };
 
 const FilmPAge = ({url}) => {
-    const {data} = useGetFilm(url);
+    const {data, isLoading, isFetching} = useGetFilm(url);
 
-    return (
-        <div>
-            <h1>{data?.title}</h1>
-            <span>
+    return isLoading
+        ? <div>"Загрузка..."</div>
+        :
+        <>
+            <div>
+                <h1>{data?.title}</h1>
+                <span>
                 <strong>episode: </strong>
-                {data?.episode_id}
+                    {data?.episode_id}
             </span>
-            <strong>description:</strong>
-            <p>{data?.opening_crawl}</p>
-        </div>
-    );
+                <strong>description:</strong>
+                <p>{data?.opening_crawl}</p>
+            </div>
+            {isFetching ? "Обновление..." : null}
+        </>;
 };
 
 export default FilmPAge;
